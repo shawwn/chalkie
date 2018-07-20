2074c2c0ae14d410da858a71a3866fa08789d2ca
Shawn Presser <shawnpresser@gmail.com>
2018-07-20 11:01:46 -0500

    Use level 3 by default

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 AnsiHTML = require('ansi-html');
 chalk = require('chalk');
 chalk.enabled = true;
-chalk.level = 2;
+chalk.level = 3;
 if (typeof module !== 'undefined') {
   module.exports = chalk;
   module.exports.html = ansiHTML;
